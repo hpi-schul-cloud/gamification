044cda533cca96d43941d9737861ba3026f9295d
Corinna Jaschek <corinna.jaschek@gmail.com>
2018-05-30 15:58:52 +0200

    add achievement model

--- src/models/achievement.js
+++ src/models/achievement.js
@@ -2,10 +2,12 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 let AchievementSchema = new Schema({
-  text: {
-    type: String,
-    required: true
-  }
+  user_id: String,
+  name: String,
+  count: Number,
+  scope: Map
 })
+
+AchievementSchema.index({ user_id: 1, name: 1 })
 
 module.exports = mongoose.model('Achievement', AchievementSchema)
