f1c20bc800f7e04fdc71550ae7a29b4e3a1eed43
Christian Flach <cmfcmf.flach@gmail.com>
2018-05-31 10:27:19 +0200

    Add events service

---
+++ src/services/event/event.service.js
@@ -0,0 +1,20 @@
+// Initializes the `Event` service on path `/events`
+const createService = require('feathers-mongoose');
+const createModel = require('../../models/event.model');
+const hooks = require('./event.hooks');
+
+module.exports = function (app) {
+  const Model = createModel(app);
+
+  const options = {
+    Model,
+  };
+
+  // Initialize our service with any options it requires
+  app.use('/events', createService(options));
+
+  // Get our initialized service so that we can register hooks
+  const service = app.service('events');
+
+  service.hooks(hooks);
+};
