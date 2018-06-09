f9e8dd56246222984e4e30d4bf5a5c64e277b0ba
Unknown <frederike.ramin@student.hpi.de>
2018-06-09 11:11:40 +0200

    Implement replaces logic

--- src/hooks/achievement-rule-checker.js
+++ src/hooks/achievement-rule-checker.js
@@ -7,7 +7,7 @@ module.exports = function (options = {}) {
   return async context => {
 
     const achievementRules = rules['achievements'];
-    // TODO max awarded + scope
+    // TODO scope
 
     for (const achievementRule of achievementRules) {
       if ( await achievementRule.canBeAwarded(context) && await achievementRule.isFulfilled(context)) {
@@ -24,9 +24,18 @@ module.exports = function (options = {}) {
         } else {
           achievementService.create({user_id: context.data.user_id, name: achievementRule.name, amount: 1});
         }
+
+        for (const replaceName of achievementRule.replaces) {
+          const replacedAchievement = await achievementService.find({
+            query: {
+              user_id: context.data.user_id,
+              name: replaceName
+            }
+          });
+          context.app.service('achievements').patch(replacedAchievement[0]._id, {amount: 0});
+        }
       }
     }
-
     return context;
   };
 };
