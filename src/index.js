ba1b1994a3c06daf38d10f1715293d932a683cad
Sebastian Kliem <sebastian-kliem@live.de>
2018-06-30 11:38:22 +0200

    use custom env vars for node-config

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ process.on('unhandledRejection', (reason, p) =>
   logger.error('Unhandled Rejection at: Promise ', p, reason)
 );
 
-app.service('amqp-connector').receiveFromQueue(process.env.RABBITMQ_HOST, app.get('rabbitQueue'));
+app.service('amqp-connector').receiveFromQueue(app.get('rabbitmq'), app.get('rabbitQueue'));
 
 server.on('listening', () => {
   logger.info('Feathers application started on http://%s:%d', app.get('host'), port);
--- src/mongoose.js
+++ src/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 module.exports = function (app) {
-  mongoose.connect(process.env.MONGO_URL, {});
+  mongoose.connect(app.get('mongodb'), {});
   mongoose.Promise = global.Promise;
 
   app.set('mongooseClient', mongoose);
