09309c58ff9e3e89fb6ab2e6f8ea695e612f4ed6
Sebastian Kliem <sebastian-kliem@live.de>
2018-07-02 20:37:17 +0200

    refactor AmqpConnector

--- src/amqp-connector.js
+++ src/amqp-connector.js
@@ -1,66 +1,75 @@
 const amqp = require('amqplib');
 class AmqpConnector {
-  constructor(app) {
+  constructor(host, app) {
+    this.host = host;
     this.app = app;
   }
 
-  async sendToQueue(host, queue, message) {
+  async connect() {
     try {
-      let conn = await amqp.connect('amqp://' + host);
-      console.info('RabbitMQ connected');
-      let ch = await conn.createChannel();
-      await ch.assertQueue(queue, {durable: true});
-      await ch.sendToQueue(queue, Buffer.from(message));
-      ch.close();
-      conn.close();
-    } catch(error) {
-      console.warn(error);
-    }
-  }
-
-  async sendToExchange(host, exchange, routingKey, message) {
-    try {
-      let conn = await amqp.connect('amqp://' + host);
-      console.info('RabbitMQ connected');
-      let ch = await conn.createChannel();
-      await ch.assertExchange(exchange, 'direct', {durable: true});
-      await ch.publish(exchange, routingKey, Buffer.from(message));
-      ch.close();
-      conn.close();
-    } catch(error) {
-      console.warn(error);
-    }
-  }
-
-  async receiveFromQueue(host, queue) {
-    try {
-      let conn = await amqp.connect('amqp://' + host);
-      console.info('RabbitMQ connected');
+      this.connection = await amqp.connect('amqp://' + this.host);
+      console.group('RabbitMQ');
+      console.info('connected');
       process.once('SIGINT', () => {
-        conn.close();
+        this.connection.close();
       });
-      let ch = await conn.createChannel();
-      await ch.assertQueue(queue, {durable: true});
-      ch.consume(queue,
-        (msg) => {
-          this.app.service('events').create(JSON.parse(msg.content));
-        },
-        {noAck: true}
-      );
-    } catch(error) {
-      console.group('rabbitmq');
+      this.channel = await this.connection.createChannel();
+      console.info('channel ready');
+      console.groupEnd('rabbitmq');
+    } catch (error) {
+      console.group('RabbitMQ');
       console.warn(error);
       console.log('reconnecting ...');
       console.groupEnd('rabbitmq');
       setTimeout(() => {
-        this.receiveFromQueue(host, queue);
+        this.connect();
       }, 1000);
+    }
+  }
+
+  async sendToQueue(queue, message) {
+    try {
+      await this.channel.assertQueue(queue, {
+        durable: true
+      });
+      await this.channel.sendToQueue(queue, Buffer.from(message));
+      this.channel.close();
+      this.connection.close();
+    } catch (error) {
+      console.warn(error);
+    }
+  }
+
+  async sendToExchange(exchange, routingKey, message) {
+    try {
+      await this.channel.assertExchange(exchange, 'direct', {
+        durable: true
+      });
+      await this.channel.publish(exchange, routingKey, Buffer.from(message));
+      this.channel.close();
+      this.connection.close();
+    } catch (error) {
+      console.warn(error);
+    }
+  }
+
+  async receiveFromQueue(queue) {
+    try {
+      await this.channel.assertQueue(queue, {
+        durable: true
+      });
+      this.channel.consume(queue,
+        (msg) => {
+          console.log(msg.content);
+          this.app.service('events').create(JSON.parse(msg.content));
+        }, {
+          noAck: true
+        }
+      );
+    } catch (error) {
+      console.warn(error);
     }
   }
 }
 
-module.exports = function (app) {
-  return new AmqpConnector(app);
-};
-
-module.exports.AmqpConnector = AmqpConnector;
+module.exports = AmqpConnector;
